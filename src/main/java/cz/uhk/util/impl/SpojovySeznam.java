46d60139accded971fc15c62eb89b6e857e4f5a4
MartinScepka <martin.scepkait@gmail.com>
2026-03-03 12:43:00 +0100

    cviceni 4, prvni ulozeni

---
+++ src/main/java/cz/uhk/util/impl/SpojovySeznam.java
@@ -0,0 +1,29 @@
+package cz.uhk.util.impl;
+
+import cz.uhk.util.Seznam;
+
+/**
+ * Genericka trida spojoveho linearniho seznamu
+ * @param <E>
+ */
+public class SpojovySeznam<E> implements Seznam<E> {
+    @Override
+    public void pridej(E prvek) {
+
+    }
+
+    @Override
+    public void smaz(int pozice) {
+
+    }
+
+    @Override
+    public E vrat(int pozice) {
+        return null;
+    }
+
+    @Override
+    public int pocet() {
+        return 0;
+    }
+}
---
+++ src/test/java/cz/uhk/util/impl/SpojovySeznamTest.java
@@ -0,0 +1,22 @@
+package cz.uhk.util.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpojovySeznamTest {
+
+    @org.junit.jupiter.api.Test
+    void pridej() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void smaz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void vrat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pocet() {
+    }
+}
